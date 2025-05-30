7a9fe15dd8a2545f273bdea45529f231a6ff5c57
leejayong <leejayong@lgupluspartners.co.kr>
2025-05-30 10:01:35 +0900

    update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://create-pome.vercel.app"}})
+# CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 def get_emotions(text):
